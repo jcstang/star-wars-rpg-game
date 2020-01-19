67f6b83beaa143d88660f578aebafd9eb3ee80c3
Jacob Stanger <jstangwork@gmail.com>
2020-01-18 20:29:59 -0700

    figured out how to get the id from the card.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,15 +10,19 @@ $(document).ready(function() {
 
   $('#player-obi').on('click', function() {
     // console.log(event.type);
-    console.log(event);
+    // console.log(event);
     // console.log(event.classList);
-    console.log(event.srcElement);
+    //console.log(event.srcElement);
 
     // NOTE: this log is the parent element,
     // so if they click on h4 or img or whatever
     // it will grab the parent element which is the card
     // TODO: capture the entire element!
-    console.log(event.srcElement.offsetParent);
+    // console.log('event.srcElement.offsetParent');
+    // console.log(event.srcElement.offsetParent);
+    console.log('event.srcElement.offsetParent.offsetParent');
+    console.log(event.srcElement.offsetParent.offsetParent);
+    console.log(event.srcElement.offsetParent.offsetParent.id);
   });
 
 });
