6f6ba6e3e5c603c50fd4cac7ed27842d4f4668af
Jacob Stanger <jstangwork@gmail.com>
2020-01-18 20:55:14 -0700

    added the player objects.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,53 @@
 
+// create players
+var obiWanKenobi = {
+  name: 'Obi-Wan Kenobi',
+  attackPower: 1,
+  defensePoser: 0,
+  attack: function() {
+    //stuff
+  },
+  increaseAttack: function() {
+    //stuff
+  }, 
+  logName: function() {
+    console.log('Name: ' + this.name);
+  }
+};
+
+obiWanKenobi.logName();
+var countDooku = Object.create(obiWanKenobi);
+countDooku.name = 'Count Dooku';
+countDooku.logName();
+
+var palps = Object.create(obiWanKenobi);
+palps.name = 'Darth Sidious';
+palps.logName();
+
+var darthMaul = Object.create(obiWanKenobi);
+darthMaul.name = 'Darth Maul';
+darthMaul.logName();
+
+
 
 $(document).ready(function() {
 
-  $('.player').on('click', function() {
-    //console.log(event);
-    //console.log(event.type);
-    //console.log(event.target);
-  });
-
   $('#player-obi').on('click', function() {
-    // console.log(event.type);
-    // console.log(event);
-    // console.log(event.classList);
-    //console.log(event.srcElement);
-
-    // NOTE: this log is the parent element,
-    // so if they click on h4 or img or whatever
-    // it will grab the parent element which is the card
-    // TODO: capture the entire element!
-    // console.log('event.srcElement.offsetParent');
-    // console.log(event.srcElement.offsetParent);
-    console.log('event.srcElement.offsetParent.offsetParent');
-    console.log(event.srcElement.offsetParent.offsetParent);
-    // this prints the parent element
-    // same as 'this'
-    console.log(event.srcElement.offsetParent.offsetParent.id);
+    console.log(this.id);
+    //TODO: move obi-wan to selected spot
+    $('.selected-character').html('<p>hello there</p>');
   });
 
   $('#player-dooku').on('click', function() {
-    console.log(this);
+    console.log(this.id);
   });
 
   $('#player-palps').on('click', function() {
-    console.log(this);
+    console.log(this.id);
   });
 
   $('#player-maul').on('click', function() {
-    console.log(this);
+    console.log(this.id);
   });
 
 });
