9887e4e28f99ffe4186b22020017b2e28bb2d3cb
Jacob Stanger <jstangwork@gmail.com>
2020-01-19 17:26:17 -0700

    first click goes on left side. 2nd goes on right. we can now select player.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -36,18 +36,55 @@ $(document).ready(function() {
     console.log(this.id);
     //TODO: move obi-wan to selected spot
     $('.selected-character').html('<p>hello there</p>');
+    // $('.battle-arena').text('obiwan');
+    var checkMySide = $('#my-side').html();
+    if (checkMySide === '') {
+      console.log('hmm it looks like it is empty')
+      $('#my-side').append( $(this).attr('class', 'col-8 container player') );
+    } else {
+      $('#opponent-side').append( $(this).attr('class', 'col-8 container player') );
+    }
+
+    // $('#myDiv2').append( $('#myDiv1>p') );
   });
 
   $('#player-dooku').on('click', function() {
     console.log(this.id);
+    var checkMySide = $('#my-side').html();
+    var checkOtherSide = $('#opponent-side').html();
+
+    if (checkMySide === '' && checkOtherSide === '') {
+      console.log('gooood... it looks like it is empty')
+      $('#my-side').append( $(this).attr('class', 'col-8 container player') );
+    } else {
+      console.log('hmmm... someone on the other side, there is.');
+      $('#opponent-side').append( $(this).attr('class', 'col-8 container player') );
+
+    }
   });
 
   $('#player-palps').on('click', function() {
     console.log(this.id);
+    var checkMySide = $('#my-side').html();
+    if (checkMySide === '') {
+      console.log('gooood... it looks like it is empty')
+      $('#my-side').append( $(this).attr('class', 'col-8 container player') );
+    } else {
+      console.log('hmmm... someone on the other side, there is.');
+      $('#opponent-side').append( $(this).attr('class', 'col-8 container player') );
+    }
   });
 
   $('#player-maul').on('click', function() {
     console.log(this.id);
+    var checkMySide = $('#my-side').html();
+    if (checkMySide === '') {
+      console.log('gooood... it looks like it is empty')
+      $('#my-side').append( $(this).attr('class', 'col-8 container player') );
+    } else {
+      console.log('hmmm... someone on the other side, there is.');
+      $('#opponent-side').append( $(this).attr('class', 'col-8 container player') );
+    }
   });
 
 });
