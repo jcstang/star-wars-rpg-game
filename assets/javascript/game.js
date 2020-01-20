2a98263428648c463c6eb69c94358ac4e3401ba3
Jacob Stanger <jstangwork@gmail.com>
2020-01-20 13:17:34 -0700

    make Player class. create 4 players.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,37 +6,56 @@ $(document).ready(function() {
   var arrayOfHealthIds = ['obi-health', 'dooku-health', 'palps-health', 'maul-health'];
   
   // create players
-  var obiWanKenobi = {
-    name: 'Obi-Wan Kenobi',
-    attackPower: 1,
-    defensePoser: 0,
-    health: 121,
-    attack: function() {
-      //stuff
-    },
-    increaseAttack: function() {
-      //stuff
-    }, 
-    logName: function() {
-      console.log('Name: ' + this.name);
+  class Player {
+    constructor(name, healthCssId, mainCssId) {
+      this.name = name;
+      this.healthCssId = healthCssId;
+      this.mainCssId = mainCssId;
+      this.attackPower = 1;
+      this.health = 121;
     }
-  };
+    displayStats() {
+      $(this.healthCssId).text(this.health);
+    }
+  }
+
+  //----
+  // var obiWanKenobi3 = {
+  //   name: 'Obi-Wan Kenobi',
+  //   healthCssId: '#obi-health',
+  //   attackPower: 1,
+  //   health: 123,
+  //   displayStats: function() {
+  //     $(this.healthCssId).text(this.health);
+  //   },
+  //   attack: function() {
+  //     //stuff
+  //   },
+  //   increaseAttack: function() {
+  //     //stuff
+  //   }, 
+  //   logName: function() {
+  //     console.log('Name: ' + this.name);
+  //   }
+  // };
+
+  var obiWanKenobi = new Player('Obi-Wan Kenobi', '#obi-health', 'player-obi');
+  console.log(obiWanKenobi);
+  obiWanKenobi.displayStats();
+
+  var countDooku = new Player('Count Dooku', '#dooku-health', 'player-dooku');
+  console.log(countDooku);
+  countDooku.displayStats();
+
+  var palps = new Player('Darth Sidious', '#palps-health', 'player-palps');
+  console.log(palps);
+  palps.displayStats();
+
+  var darthMaul = new Player('Darth Maul', '#maul-health', 'player-maul');
+  console.log(darthMaul);
+  darthMaul.displayStats();
   
-  obiWanKenobi.logName();
-  // $('#obi-health').text(obiWanKenobi.heath);
-  displayAllPlayerStats(arrayOfHealthIds);
   
-  var countDooku = Object.create(obiWanKenobi);
-  countDooku.name = 'Count Dooku';
-  countDooku.logName();
-  
-  var palps = Object.create(obiWanKenobi);
-  palps.name = 'Darth Sidious';
-  palps.logName();
-  
-  var darthMaul = Object.create(obiWanKenobi);
-  darthMaul.name = 'Darth Maul';
-  darthMaul.logName();
 
 
 // ========================================
@@ -87,12 +106,8 @@ $(document).ready(function() {
 
   }
 
-  function displayAllPlayerStats(arr) {
-    for (let i = 0; i < arr.length; i++) {
-      
-      
-    }
-    $('#obi-health').text(obiWanKenobi.health);
+  function displayPlayerStats(player) {
+    $(player.healthCssId).text(player.health);
   }
 
 });
